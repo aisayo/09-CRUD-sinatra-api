a2c0dc4a4e409f7d0946a22f107848ee447e677f
Aysan Isayo <aisayo@icloud.com>
2021-10-04 12:05:32 -0700

    lecture code

--- demo-react/src/StarshipsContainer.js
+++ demo-react/src/StarshipsContainer.js
@@ -1,5 +1,82 @@
 function StarshipsContainer() {
 
+  // write the event handlers 
+
+  // a request for the index endpoint: receive all starship records
+
+  // get invoked upon a component mounting 
+  const getStarships = () => {
+    fetch("http://localhost:9292/starships")
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }
+
+  // what about post request? What point in apps lifecycle
+  // Create a new starship form has been submitted
+
+  const createStarship = (formData) => {
+
+    // formData is going to be an object with k,v pairs
+    const { name, color, model } = formData 
+    
+    const starship = {
+      name, 
+      model,
+      color
+    }
+
+    const configObj = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(starship)
+    }
+
+    fetch("http://localhost:9292/starships", configObj)
+    .then(resp => resp.json())
+    .then(starship => console.log(starship))
+  }
+
+
+  // Upon a click on a starship tile, to show particular starship details(show page)
+  const getStarship = (id) => {
+
+    fetch(`http://localhost:9292/starships/${id}`)
+    .then(resp => resp.json())
+    .then(starship => console.log(starship))
+  }
+
+
+  // this event handler will be invoked when an edit form is submitted
+
+  const updateStarship = (formData, starshipId) => {
+
+    // formData is going to be an object with k,v pairs
+    const { name, color, model } = formData 
+    
+    const starship = {
+      name, 
+      model,
+      color
+    }
+
+    const configObj = {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(starship)
+    }
+
+    fetch(`http://localhost:9292/starships/${starshipId}`, configObj)
+    .then(resp => resp.json())
+    .then(starship => console.log(starship))
+  }
+
+
+  debugger;
+
   return (
     <div>
       StarshipsContainer Component
--- demo/app/controllers/starships_controller.rb
+++ demo/app/controllers/starships_controller.rb
@@ -1,15 +1,34 @@
 class StarshipsController < ApplicationController
 
+    # index / read 
     get '/starships' do 
         Starship.all.to_json
     end 
 
+    # create
     post '/starships' do 
-        starship = Starship.create(
-            name: params[:name],
-            model: params[:model],
-            color: params[:color]
-        )
+        # starship = Starship.create(
+        #     name: params[:name],
+        #     model: params[:model],
+        #     color: params[:color]
+        # )
+        starship = Starship.create(params)
+        starship.to_json
+    end 
+
+    # read action / show: showing 1 resource details 
+
+    get '/starships/:id/:query' do 
+        # find the starship with the given id 
+        # respond with json format of that object
+        starship = Starship.find(params[:id])
+        starship.to_json
+    end 
+
+    patch '/starships/:id' do 
+        binding.pry
+        starship = Starship.find(params[:id])
+        starship.update(params)
         starship.to_json
     end 
 
